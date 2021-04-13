a528eb7a09dbb1506a2868cbf8c48608f695f96f
AlexisH1project <projects.h1mx@gmail.com>
2021-04-13 14:43:16 -0500

    verificara para que es este archivo no recuerdo que se queria hacer

--- roles/Controller/controllerDescargaPagina2.php
+++ roles/Controller/controllerDescargaPagina2.php
@@ -3,6 +3,7 @@
 $nombreDeArchivoDescarga = $_GET['nombreDecarga'];
 $tipoArchivo = $_GET['extencion'];
 $ruta =	explode("/", $nombreDeArchivoDescarga); 
+$from2 = '\\\\PWIDGRHOSISFO01\\pdfs2\\';
 
 	if($tipoArchivo == "zip" || $tipoArchivo == "ZIP" || $tipoArchivo == "7z" || $tipoArchivo == "7Z" || $tipoArchivo == "RAR" || $tipoArchivo == "rar" || $tipoArchivo == "gz"|| $tipoArchivo == "GZ"){
 		header("Content-type: application/zip");
@@ -20,20 +21,39 @@ $ruta =	explode("/", $nombreDeArchivoDescarga);
         readfile("./DOCUMENTOS_PDC/".$nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
 
 	}else{
-		header("Content-type: application/PDF");
+		// header("Content-type: application/PDF");
 		echo $nombreDeArchivoDescarga;
+		$sqlQna = "SELECT * FROM m1ct_fechasnomina WHERE estadoActual = 'abierta'";
+		if($resQna = mysqli_query($conexion,$sqlQna)){
+			$rowQna = mysqli_fetch_row($resQna);
+			// $fehaI = date("d-m-Y", strtotime($rowQna[4])); 
+			// $fehaF = date("d-m-Y", strtotime($rowQna[5])); 
+			$newQna = $rowQna[0];
+		}
+		
+		echo $ruta[1];
 		if($ruta[1] == "DOCUMENTOS_MOV"){
 			readfile($nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
 		}
-		if($ruta == "DOCUMENTOS_BAJAS"){
+		if($ruta[1] == "DOCUMENTOS_BAJAS"){
 			readfile($nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
-		}if($ruta == "DOCUMENTOS_MOV_QR"){
-			readfile($nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
+		}if($ruta[1] == "DOCUMENTOS_MOV_QR"){
+			$hoy = "select CURDATE()";
+			$tiempo ="select curTime()";
+			if ($resultHoy = mysqli_query($conexion,$hoy) AND $resultTime = mysqli_query($conexion,$tiempo)) {
+				$row = mysqli_fetch_row($resultHoy);
+				$fecha = str_replace ( "-", '',$row[0] ); 
+			}
+			$elCurp= explode("_", $ruta[3]);
+			$sqlRfc = "SELECT * FROM conteo_qr WHERE curp = '$elCurp[0]'" ;
+			$resultConteo = mysqli_query($conexion, $sqlRfc);
+			$rowConteo = mysqli_fetch_assoc($resultConteo);
+			copy($from2.$row[7].".pdf" , $nombreDeArchivoDescarga);
+			// readfile($nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
 		}else{
-			readfile("./DOCUMENTOS/".$nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
-			readfile($nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
-			readfile("./DOCUMENTOS_RES/".$nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
-		// readfile("./DOCUMENTOS_PDC/".$nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
+			// readfile("./DOCUMENTOS/".$nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
+			// readfile($nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
+			// readfile("./DOCUMENTOS_RES/".$nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
 		}
 
 	}
