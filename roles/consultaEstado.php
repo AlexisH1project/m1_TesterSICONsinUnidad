cbc032bba0dabfb73f1cd43aa47eff2f2ca60e5a
VictorJaramilloC <victorstrike93@gmail.com>
2021-04-18 00:51:41 -0500

    Barra superior de busqueda en consultaEstado y nueva pagina buscarRegistro donde se muestra la tabla de la busqueda, se realizan filtros y se genera excel

--- roles/consultaEstado.php
+++ roles/consultaEstado.php
@@ -13,7 +13,7 @@
 	<!-- 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script type="text/javascript" src="./jquery/jquery.tabledit.js"></script>
 		<script type="text/javascript" src="./jquery/jquery.tabledit.min.js"></script> -->
-
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 		<link href='css/jquery-ui.min.css' type='text/css' rel='stylesheet'>
 		<link href='css/jquery-ui.css' type='text/css' rel='stylesheet'>
 
@@ -57,8 +57,91 @@ tbody {
 			max-width: 151px;
 		}
 
+.container-4 input#search{
+  width: 130px;
+  height: 30px;
+  background: #fff;
+  border: none;
+   font-size: 10pt;
+ font-family: 'Arial', sans-serif;
+ font-weight: 400;
+  float: left;
+  color: #00000;
+  padding-left: 15px;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
+}
 
+.container-4 button.icon{
+  -webkit-border-top-right-radius: 5px;
+  -webkit-border-bottom-right-radius: 5px;
+  -moz-border-radius-topright: 5px;
+  -moz-border-radius-bottomright: 5px;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+ 
+  border: none;
+  background: #fff;
+  height: 30px;
+  width: 50px;
+  color: #4f5b66;
+  opacity: 0;
+  font-size: 10pt;
+ 
+  -webkit-transition: all .55s ease;
+  -moz-transition: all .55s ease;
+  -ms-transition: all .55s ease;
+  -o-transition: all .55s ease;
+  transition: all .55s ease;
+}
 
+.container-4:hover button.icon, .container-4:active button.icon, .container-4:focus button.icon{
+    outline: none;
+    opacity: 1;
+    margin-left: -50px;
+  }
+ 
+  .container-4:hover button.icon:hover{
+    background: white;
+  }
+
+  .select-css {
+ display: block;
+ font-size: 10pt;
+ font-family: 'Arial', sans-serif;
+ height: 30px;
+ font-weight: 400;
+ color: #4f5b66;
+ line-height: 1.3;
+ padding: .4em 1.4em .3em .8em;
+ width: 110px;
+ max-width: 100%; 
+ box-sizing: border-box;
+ margin: 0;
+ border: 1px solid #aaa;
+ box-shadow: 0 1px 0 1px rgba(0,0,0,.03);
+ border-radius: .3em;
+ -moz-appearance: none;
+ -webkit-appearance: none;
+ appearance: none;
+ background-color: #fff;
+ background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+   linear-gradient(to bottom, #ffffff 0%,#f7f7f7 100%);
+ background-repeat: no-repeat, repeat;
+ background-position: right .7em top 50%, 0 0;
+ background-size: .65em auto, 100%;
+}
+.select-css::-ms-expand {
+ display: none;
+}
+.select-css:hover {
+ border-color: #888;
+}
+
+.select-css option {
+ font-weight:normal;
+}
 
 		  </style>
 
@@ -127,6 +210,7 @@ tbody {
 
 
 	</head>
+		<br>
 	<body onload="nobackbutton();">
 			<?php
 				include "configuracion.php";
@@ -139,6 +223,8 @@ tbody {
 
 			?>
 			<br>
+
+	
 		  				<a  href= <?php echo ("'./menuPrincipal.php?usuario_rol=$usuarioSeguir'");?>><img class="img-responsive" src="img/ss1.png" height="90" width="280"/></a>
 
 			<?php
@@ -154,8 +240,62 @@ tbody {
 		<nav class="navbar fixed-top navbar-expand-lg navbar-dark plantilla-input fixed-top">
 		    <div class="container">
 		      <div class="collapse navbar-collapse" id="navbarResponsive">
-		        <ul class="navbar-nav ml-auto">          
-		        
+
+		        <ul class="navbar-nav ml-auto">  
+		     <li class="nav-item">    
+		        	<section class="search-sec">
+    <div class="container">
+        <form enctype="multipart/form-data" action="buscarRegistro.php" method="get">
+            <div class="row">
+            	<input type='text' style='display: none;' name='usuario_rol' value='$usuarioSeguir'>
+                <div class="col-lg-12">
+                    <div class="row">
+                    	<div class="container-4">
+                         
+                            <select class="select-css" id="elanio" name="elanio">
+                                <option value="">AÑO</option>
+                                <option value="2019">2019</option>
+                                <option value="2020">2020</option>
+                                <option value="2021">2021</option>
+                            </select>
+              
+                    </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <select class="select-css" id="laqnia" name="laqnia">
+                                <option>QUINCENA</option>
+                              	
+													<?php
+													if (!$conexion->set_charset("utf8")) {//asignamos la codificación comprobando que no falle
+													       die("Error cargando el conjunto de caracteres utf8");
+													}
+
+													$consulta = "SELECT * FROM m1ct_fechasnomina";
+													$resultado = mysqli_query($conexion , $consulta);
+													$contador=0;
+
+													while($misdatos = mysqli_fetch_assoc($resultado)){ $contador++;?>
+													<option value="<?php echo $misdatos["id_qna"]; ?>"><?php echo $misdatos["id_qna"]; ?></option>
+													<?php 
+													$analista = $misdatos["nombrePersonal"];
+
+												}?>          
+                            </select>
+                        </div>
+                       
+                        <div class="box">
+  <div class="container-4">
+    <input type="search" id="search" name="search" placeholder="Busqueda" />
+    <button class="icon"><i class="fa fa-search"></i></button>
+  </div>
+</div>
+
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
+</li>  
 		          <li class="nav-item">
 		            <a class="nav-link" href='../LoginMenu/vista/cerrarsesion.php'>CERRAR SESIÓN</a>
 		          </li>
@@ -283,8 +423,7 @@ tbody {
 	</div>
 					
 		<br>
-		<br>
-		<br>
+		
 
 				
 				<?php 
