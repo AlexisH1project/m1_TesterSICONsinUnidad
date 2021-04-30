9b2c9539cd5ce1216f48b61de02df6c09bb4a33c
AlexisH1project <projects.h1mx@gmail.com>
2021-04-29 20:13:42 -0500

    ✔  ya esta en el servido

--- roles/Controller/controllerDescargaPagina2.php
+++ roles/Controller/controllerDescargaPagina2.php
@@ -21,16 +21,8 @@ $from2 = '\\\\PWIDGRHOSISFO01\\pdfs2\\';
         readfile("./DOCUMENTOS_PDC/".$nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
 
 	}else{
-		// header("Content-type: application/PDF");
-		echo $nombreDeArchivoDescarga;
-		$sqlQna = "SELECT * FROM m1ct_fechasnomina WHERE estadoActual = 'abierta'";
-		if($resQna = mysqli_query($conexion,$sqlQna)){
-			$rowQna = mysqli_fetch_row($resQna);
-			// $fehaI = date("d-m-Y", strtotime($rowQna[4])); 
-			// $fehaF = date("d-m-Y", strtotime($rowQna[5])); 
-			$newQna = $rowQna[0];
-		}
-		
+		header("Content-type: application/PDF");
+        header("Content-Transfer-Encoding: binary");
 		echo $ruta[1];
 		if($ruta[1] == "DOCUMENTOS_MOV"){
 			readfile($nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
@@ -38,18 +30,18 @@ $from2 = '\\\\PWIDGRHOSISFO01\\pdfs2\\';
 		if($ruta[1] == "DOCUMENTOS_BAJAS"){
 			readfile($nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
 		}if($ruta[1] == "DOCUMENTOS_MOV_QR"){
-			$hoy = "select CURDATE()";
-			$tiempo ="select curTime()";
-			if ($resultHoy = mysqli_query($conexion,$hoy) AND $resultTime = mysqli_query($conexion,$tiempo)) {
-				$row = mysqli_fetch_row($resultHoy);
-				$fecha = str_replace ( "-", '',$row[0] ); 
-			}
-			$elCurp= explode("_", $ruta[3]);
-			$sqlRfc = "SELECT * FROM conteo_qr WHERE curp = '$elCurp[0]'" ;
-			$resultConteo = mysqli_query($conexion, $sqlRfc);
-			$rowConteo = mysqli_fetch_assoc($resultConteo);
-			copy($from2.$row[7].".pdf" , $nombreDeArchivoDescarga);
-			// readfile($nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
+			// $hoy = "select CURDATE()";
+			// $tiempo ="select curTime()";
+			// if ($resultHoy = mysqli_query($conexion,$hoy) AND $resultTime = mysqli_query($conexion,$tiempo)) {
+			// 	$row = mysqli_fetch_row($resultHoy);
+			// 	$fecha = str_replace ( "-", '',$row[0] ); 
+			// }
+			// $elCurp= explode("_", $ruta[3]);
+			// $sqlRfc = "SELECT * FROM conteo_qr WHERE curp = '$elCurp[0]'" ;
+			// $resultConteo = mysqli_query($conexion, $sqlRfc);
+			// $rowConteo = mysqli_fetch_assoc($resultConteo);
+			// copy($from2.$row[7].".pdf" , $nombreDeArchivoDescarga);
+			readfile($nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
 		}else{
 			// readfile("./DOCUMENTOS/".$nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
 			// readfile($nombreDeArchivoDescarga); //C:/xampp2/htdocs/SICON_w/roles/Controller/
